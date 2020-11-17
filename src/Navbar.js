b2f59af5c94d1216be8f9e18a868040a6a95aafe
Ooi Ren An <ooira12345@yahoo.com.sg>
2020-11-17 23:18:04 +0000

    Navbar Basic

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,9 @@ import './Navbar.css';
 function Navbar() {
   return (
     <div className="Navbar">
-      <div className="NavbarTitle"><h3>Navbar Testing</h3></div>
+      <div className="NavbarTitle"><h3>Contact Tracing App</h3></div>
+      <br/>
+      <div><button className="button">Log In</button></div>
     </div>
   );
 }
