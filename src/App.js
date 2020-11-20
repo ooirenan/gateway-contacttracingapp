17c83681bd9d1ca74cb292e1d5fc764e6c854399
cheezanity <65487560+cheezanity@users.noreply.github.com>
2020-11-20 23:11:46 +0000

    Log In Form 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Meeting from './Meeting';
+import Popup from './components/popup'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Meeting />
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      showPopup: false
+    };
+  };
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <Meeting />
+        <button onCLick={this.togglePopup.bind(this)}>show popup</button>
+        {this.state.showPopup ? (
+        <Popup text="Close Me" closePopup={this.togglePopup.bind(this)} />
+        ) : null}
 
       <h3>Hackbridge Gateway - Contact Tracing App</h3>
 
 
     </div>
   );
+}
 }
 
 export default App;
